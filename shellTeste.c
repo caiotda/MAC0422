e678935782f5a80c8432f805ae58492f2478ae4c
Caio Andrade <caiotdandrade@gmail.com>
2019-09-10 09:57:22 -0300

    Writes basic version of a shell

--- shellTeste.c
+++ shellTeste.c
@@ -10,12 +10,32 @@ char prompt[] = ">> ";
 
 int main (int argc, char **argv) {
 	char* cmdline = malloc(sizeof(char) * MAXLINE);
+	char path[256];
+	char command[64];
+	int i, j, has_found_space;
 
 	while(1) {
 		printf("%s", prompt);
 
 		cmdline = fgets(cmdline, MAXLINE, stdin);
-		printf("Voce digitou : %s\n", cmdline);		
+		for (i = 0, has_found_space = 0, j = 0; cmdline[i] != '\n'; i++) {
+			if (cmdline[i] == ' ') {
+				command[i] = '\0';
+				has_found_space = 1;
+				i++;
+			}
+			if (!has_found_space)
+				command[i] = cmdline[i];
+			else {
+				path[j] = cmdline[i];
+				j++;
+			}
+		}
+
+		path[j] = '\0';
+		printf("Seu comando foi %s\n", command);
+		printf("E o caminho dado foi %s\n", path);
+
 		if((cmdline == NULL) && ferror(stdin))
 			printf("Erro no fgets");
 
